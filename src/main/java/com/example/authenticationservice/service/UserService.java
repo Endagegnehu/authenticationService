f88c854dd6c8cce4da625c621aba20b370d5fb82
endagegnehu <endagegnehu@gmail.com>
2021-10-10 21:12:07 -0500

    auth service and getway

---
+++ src/main/java/com/example/authenticationservice/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.authenticationservice.service;
+
+import com.example.authenticationservice.domain.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UserService {
+    public void addUser(User user);
+    public Optional<User> findUserByFirstName(String firstName);
+
+    public UserDetails getUserDetails(String firstName);
+}
---
+++ src/main/java/com/example/authenticationservice/util/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.example.authenticationservice.util;
+
+public enum PaymentMethod {
+     PAYPAL, CREDITCARD,BANKACCOUNT
+}
